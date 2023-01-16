3ac46d0e807b37f405950bfb4beaa64db3035387
Rimantas Zogla <rimaszogla@gmail.com>
2023-01-16 09:21:02 +0200

    initial commit

---
+++ JS/01-console.js
@@ -0,0 +1,16 @@
+console.clear();
+
+//single line comment
+
+/*
+
+    multi 
+    line 
+    comment
+*/
+
+console.log(5);
+console.log(5, 12);
+console.log(5.15);
+console.log(5, 2, 3, 9, 5, 7);
+console.log(5);
